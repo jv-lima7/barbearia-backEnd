5a1ec6afad5fc381ec7e51971fbe4fb383dfd7be
Joao Vitor <joaovitor4015@outlook.com>
2026-05-31 12:40:51 -0300

    corrigindo codigo

--- src/main/java/com/barbearia/application/config/CorsConfig.java
+++ src/main/java/com/barbearia/application/config/CorsConfig.java
@@ -10,7 +10,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins(
+                        "https://jv-lima7.github.io",
+                        "http://127.0.0.1:5500"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
